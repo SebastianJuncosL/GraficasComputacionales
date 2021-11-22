523dc2522f559e4eef4107eb055802c07656a5ef
Sebastian Juncos <sebastianjuncos@gmail.com>
2021-11-22 12:38:42 -0600

    Finished drop box

--- Actividad/agente.py
+++ Actividad/agente.py
@@ -1,19 +1,23 @@
 from mesa import Agent
 
+
 class Delivery(Agent):
     # Donde van a poner las cajas.
     def __init__(self, pos, model):
         super().__init__(pos, model)
         self.pos = pos
-    
+
     def step(self):
         pass
 
+
 class Box(Agent):
     # Cajas a recolectar.
-    def __init__(self, unique_id, model):
+    def __init__(self, unique_id, model, delivery_pos):
         super().__init__(unique_id, model)
         self.unique_id = unique_id
+        self.delivery_pos = delivery_pos
+
 
 class Collector(Agent):
     # Persona que recojera cajas.
@@ -23,7 +27,7 @@ class Collector(Agent):
         self.pos = pos
         self.delivery_pos = delivery_pos
         self.box = -1
-    
+
     def step(self):
         possible_steps = self.model.grid.get_neighborhood(
             self.pos,
@@ -31,7 +35,7 @@ class Collector(Agent):
             include_center=False)
         possible_box = []
 
-        #check for obstacles
+        # check for obstacles
         for i in range(len(possible_steps)):
             cell = self.model.grid.get_cell_list_contents([possible_steps[i]])
             if (cell > 0):
@@ -39,9 +43,6 @@ class Collector(Agent):
                     possible_steps.pop(i)
                 elif(isinstance(cell[0], Box)):
                     possible_box.append(possible_steps.pop(i))
-        
         # Drop box.
         if (self.box != -1 and self.pos == self.delivery_pos):
-            
-
-
+            self.box = -1
--- Actividad/flask_server.py
+++ Actividad/flask_server.py
@@ -7,11 +7,13 @@ app = Flask("Coordinate server")
 num_agents = 0
 limit = 10
 
+
 def random_point():
     """Generate a new 3D coordinate"""
     return {"x": uniform(-limit, limit),
             "y": uniform(-limit, limit),
             "z": uniform(-limit, limit)}
+
 
 @app.route("/")
 def default():
@@ -19,13 +21,15 @@ def default():
     print("Recived a request at /")
     return "This is working"
 
+
 @app.route("/config", methods=['POST'])
 def configure():
     """Set up the simulation"""
-    global num_agents 
+    global num_agents
     num_agents = int(request.form.get("numAgents"))
     print(f"Recived num_agents = {num_agents}")
     return jsonify({"OK": num_agents})
+
 
 @app.route("/update", methods=['GET'])
 def update_position():
