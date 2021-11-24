d34b4c79e3326bffeb1b7f161f0985d34807958a
SkohnE <s.kohne99@gmail.com>
2021-11-23 22:32:20 -0600

    agrego modelo

---
+++ Actividad/model.py
@@ -0,0 +1,40 @@
+from mesa.time import SimultaneousActivation
+from mesa.space import MultiGrid
+from mesa import Model
+from agent import Delivery, Box, Collector
+
+class CollectorsModel(Model):
+    def __init__(self, N, N_boxes,  height, width):
+        self.num_agents = N
+        self.grid = MultiGrid(width, height, torus = False) 
+        self.schedule = SimultaneousActivation(self)
+        self.running = True 
+        
+        pos_gen = lambda w, h: (self.random.randrange(w), self.random.randrange(h))
+        # Add Delivery to a random empty grid cell
+        delivery_pos = pos_gen(self.grid.width, self.grid.height)
+        a = Delivery(delivery_pos, self)
+        
+        # Add the agent to a random empty grid cell
+        for i in range(self.num_agents):
+            pos = pos_gen(self.grid.width, self.grid.height)
+            while (not self.grid.is_cell_empty(pos)):
+                pos = pos_gen(self.grid.width, self.grid.height)
+
+            a = Collector(i+1000, pos, delivery_pos, self) 
+            self.schedule.add(a)
+            self.grid.place_agent(a, pos)
+        
+        for i in range(N_boxes):
+            pos_gen = lambda w, h: (self.random.randrange(w), self.random.randrange(h))
+
+            pos = pos_gen(self.grid.width, self.grid.height)
+            while (not self.grid.is_cell_empty(pos)):
+                pos = pos_gen(self.grid.width, self.grid.height)
+
+            a = Box(i+2000, pos, delivery_pos, self) 
+            self.schedule.add(a)
+            self.grid.place_agent(a, pos)
+    
+    def step(self):
+        self.schedule.step()
